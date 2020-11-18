ae6062883fe66f0b381cc4d28129cac2ae77ff43
unknown <rajeskannusamy@gmail.com>
2020-11-18 19:45:35 +0530

    message

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+	<Navbar fixed="bottom" expand="lg" bg="dark" variant="dark">
+		<p className="m-auto footerText">Copyright &copy; 2020</p>
+	</Navbar>
+  );
+}
+
+export default Footer;
