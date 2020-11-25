24cb4e06490210bcf50ccb67773c7159a0151a2b
unknown <rajeskannusamy@gmail.com>
2020-11-25 17:03:27 +0530

    updated the details

---
+++ src/tests/about.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import About from '../components/about';
+
+describe('About page snapshot', () => {
+    let wrapper;
+    let props;
+    beforeEach(() => {
+        wrapper = mount(<About {...props} />);
+    });
+
+    
+    test('renders correctly', () => {
+        expect(wrapper.debug()).toMatchSnapshot();
+    });
+});
